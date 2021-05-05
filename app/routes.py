ac442b8be523671c02215ff0668df85dd187f4d8
David <alajju4@naver.com>
2021-05-06 02:15:04 +0900

    9

--- app/routes.py
+++ app/routes.py
@@ -11,13 +11,18 @@ from datetime import datetime
 from app.forms import EditProfileForm
 from app.forms import EmptyForm
 
-@app.route('/')
-
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
-    user = {'username': 'Miguel'}
-    posts = [
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post is now live!')
+        return redirect(url_for('index'))
+    posts = current_user.followed_posts().all()[
         {
             'author': {'username': 'John'},
             'body': 'Beautiful day in Portland!'
@@ -27,7 +32,10 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     ]
-    return render_template("index.html", title='Home Page', posts=posts)
+    return render_template("index.html", title='Home Page', form=form,
+                           posts=posts)
+        
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -128,3 +136,9 @@ def user(username):
     # ...
     form = EmptyForm()
     return render_template('user.html', user=user, posts=posts, form=form)
+
+@app.route('/explore')
+@login_required
+def explore():
+    posts = Post.query.order_by(Post.timestamp.desc()).all()
+    return render_template('index.html', title='Explore', posts=posts)
