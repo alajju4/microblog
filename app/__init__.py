7b2c4e6343beb9e87209d363ea5255443b0b9340
David <alajju4@naver.com>
2021-05-18 15:57:08 +0900

    13

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,4 @@
 #템플릿 등의 파일들을 로드 
-
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
@@ -10,8 +9,9 @@ from flask_login import LoginManager
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
-from flask_babel import Babel, lazy_gettext as _l
 from config import Config
+from flask_babel import Babel, lazy_gettext as _l
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
